eb4dc50cdd071b8c0d78e463480af1e5b9b5fea0
jqiwen <jqiwen00@gmail.com>
2025-02-15 16:32:42 -0500

    update pvp

--- src/frontend/app/(pages)/ginrummy/pvp/page.tsx
+++ src/frontend/app/(pages)/ginrummy/pvp/page.tsx
@@ -10,7 +10,7 @@ export default function pvp() {
     
         {/* <div className="w-full h-full flex">  */}
         <div className="w-full pt-[100px] flex flex-col items-center justify-center">
-            <CardWithForm></CardWithForm>
+            <PVP></PVP>
         </div>
         {/* </div> */}
     </div>
@@ -44,45 +44,67 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
-export function CardWithForm() {
-
-
+export function PVP() {
     const [isCreatingNewRoom, setIsCreatingNewRoom] = useState(false);
+
+    const [currentTitle, setCurrentTitle] = useState('Join a Room')
+    const [currentButton, setCurrenButton] = useState('Join')
+
 
     useEffect(() => {
         console.log(isCreatingNewRoom);
-    })    
+        if (isCreatingNewRoom) {
+            setCurrentTitle('Create a Room')
+            setCurrenButton('Create')
+        } else{
+            setCurrentTitle('Join a Room')
+            setCurrenButton('Join')
 
-    function handleSwitch(){
-        setIsCreatingNewRoom(!isCreatingNewRoom)
+        }
+    }, [isCreatingNewRoom]); 
+
+    function handleButton(){
+        // if (isCreatingNewRoom) {
+        //     alert('Create')
+        // } else{
+        //     alert('Join')
+        // }
+    }
+
+    function handleSwitch(checked: boolean) {
+        setIsCreatingNewRoom(checked);
     }
     
     return (
         <Card className="w-[350px]">
             <CardHeader>
-                <CardTitle>Join a room </CardTitle>
-                <CardDescription>Switch to create a new room if you want to create a new one.</CardDescription>
+                <CardTitle>{currentTitle}</CardTitle>
+                <CardDescription>Switch to create if you are the holder.</CardDescription>
             </CardHeader>
             <CardContent>
                 <form>
-                <div className="grid w-full items-center gap-4">
-                <div className="flex items-center space-x-2">
-                <Switch id="isCreatingNewRoom" 
-                     />
-                <Label htmlFor="isCreatingNewRoom">Create a new room</Label>
-            </div>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex items-center space-x-2">
+                            <Switch
+                                id="isCreatingNewRoom"
+                                checked={isCreatingNewRoom} 
+                                onCheckedChange={handleSwitch} 
+                            />
+                            <Label htmlFor="isCreatingNewRoom">Create</Label>
+                        </div>
 
-                <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Room NO.</Label>
-                <Input id="number" placeholder="enter number" />
-                </div>
-            </div>
-            </form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Join</Button>
-        </CardFooter>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="number">Room NO.</Label>
+                            <Input id="number" placeholder="Enter number" />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button variant={'outline'}>Back</Button>
+                <Button onClick={handleButton}>{currentButton}</Button>
+            </CardFooter>
         </Card>
-    )
+    );
 }
+

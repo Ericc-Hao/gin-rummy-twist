5ad3b32d37530d0c8d614a1fe5ec9781ba2f6618
Chenrui Hao <erichaocr@gmail.com>
2025-03-20 18:00:03 -0400

    fix(Backend): update version to 0.0.1-rc1 in status template

--- src/backend/app.py
+++ src/backend/app.py
@@ -21,7 +21,7 @@ server_start_time = datetime.datetime.now()
 @app.route("/")
 def index():
     return render_template("status.html", 
-                           version = "0.0.1-rc0",
+                           version = "0.0.1-rc1",
                            start_time = server_start_time.timestamp())
 
 @app.route('/api/signup', methods=['POST'])
